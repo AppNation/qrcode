7d05d504773ff9cc186b40fe5b08a40e663e2354
Yalcin Ozdemir <yalcinozdemir2@gmail.com>
2021-07-01 18:23:36 +0300

    Update QRCaptureView.m

--- qrcode/ios/Classes/QRCaptureView.m
+++ qrcode/ios/Classes/QRCaptureView.m
@@ -53,9 +53,7 @@
             
             output.metadataObjectTypes = output.availableMetadataObjectTypes;
             [output setMetadataObjectsDelegate:self queue:dispatch_get_main_queue()];
-            [output setMetadataObjectTypes:@[AVMetadataObjectTypeUPCECode, AVMetadataObjectTypeCode39Code, AVMetadataObjectTypeCode39Mod43Code,
-            AVMetadataObjectTypeEAN13Code, AVMetadataObjectTypeEAN8Code, AVMetadataObjectTypeCode93Code, AVMetadataObjectTypeCode128Code,
-            AVMetadataObjectTypePDF417Code, AVMetadataObjectTypeQRCode, AVMetadataObjectTypeAztecCode]];
+            [output setMetadataObjectTypes:@[AVMetadataObjectTypePDF417Code]];
             
             dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^{
                   [self.session startRunning];
